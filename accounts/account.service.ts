cf0bcacb51621cf4a0c33ff1ca33ba99ff18b278
delcuyos <DelCuyos>
2026-05-19 09:08:02 +0800

    change the verification token

--- accounts/account.service.ts
+++ accounts/account.service.ts
@@ -86,7 +86,7 @@ async function register(params: any, origin: any) {
   
   const isFirstAccount = (await db.Account.count()) === 0;
   account.role = isFirstAccount ? Role.Admin : Role.User;
-  account.verificationToken = randomTokenString();
+  account.verified = new Date();
   
   account.passwordHash = await hash(params.password);
 
